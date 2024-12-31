90305feadda276c6c7708d65d83b42bb214fece5
Ryan George <Ryan@qdds.nl>
2024-12-31 10:56:40 +0100

    chore(client): clean up app entry

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,25 @@
 import React, { useEffect, useMemo } from "react";
 import { DeskThing } from "deskthing-client";
 import { SocketData } from "deskthing-client/dist/types";
-import { useActorRef, useSelector } from "@xstate/react";
-import entityManagerMachine, {
-	EntityMachineActor,
-} from "./state/entityManagerMachine";
 import Startup from "./components/startup/Startup";
+import Grid from "./components/grid/Grid";
+import { entityManagerActor } from "./state/entityManagerMachine";
+import { useSelector } from "@xstate/react";
+import Entity from "./components/entity/Entity";
 
 const deskthing = DeskThing.getInstance();
 
-const Entity = ({ id, actor }: { id: string; actor: EntityMachineActor }) => {
-	const entityState = useSelector(actor, (snapshot) => snapshot.context?.state);
-
-	return (
-		<div>
-			{id} - {entityState}
-		</div>
-	);
-};
-
 const App: React.FC = () => {
-	const actorRef = useActorRef(entityManagerMachine);
-
-	const refs = useSelector(actorRef, (snapshot) => snapshot.context.refs);
+	const refs = useSelector(
+		entityManagerActor,
+		(snapshot) => snapshot.context.refs
+	);
 
 	useEffect(() => {
 		const onAppData = async (data: SocketData) => {
 			switch (data.type) {
 				case "homeassistant_data":
-					actorRef.send({
+					entityManagerActor.send({
 						type: "ENTITIES_CHANGE",
 						entities: data.payload,
 					});
@@ -42,15 +33,17 @@ const App: React.FC = () => {
 	}, []);
 
 	const entities = useMemo(() => {
-		return Object.entries(refs);
+		return Object.values(refs);
 	}, [refs]);
 
 	return (
 		<div className="bg-slate-800 w-screen h-screen">
 			<Startup />
-			{entities.map(([entityId, actor]) => (
-				<Entity id={entityId} key={entityId} actor={actor} />
-			))}
+			<Grid>
+				{entities.map((entity) => (
+					<Entity machine={entity} />
+				))}
+			</Grid>
 		</div>
 	);
 };
