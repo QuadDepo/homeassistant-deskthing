071d585ad46045c0adccd73a588d7a13a9b02b02
Ryan George <Ryan@qdds.nl>
2025-01-02 10:55:48 +0100

    chore(client): use base entity

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import React, { useEffect, useMemo } from "react";
-import { DeskThing } from "deskthing-client";
 import { SocketData } from "deskthing-client/dist/types";
 import Startup from "./components/startup/Startup";
 import Grid from "./components/grid/Grid";
 import { entityManagerActor } from "./state/entityManagerMachine";
 import { useSelector } from "@xstate/react";
-import Entity from "./components/entity/Entity";
-
-const deskthing = DeskThing.getInstance();
+import BaseEntity from "./components/entity/BaseEntity";
+import deskthing from "./Deskthing";
 
 const App: React.FC = () => {
 	const refs = useSelector(
@@ -41,7 +39,7 @@ const App: React.FC = () => {
 			<Startup />
 			<Grid>
 				{entities.map(([id, entity]) => (
-					<Entity id={id} machine={entity} />
+					<BaseEntity id={id} machine={entity} />
 				))}
 			</Grid>
 		</div>
