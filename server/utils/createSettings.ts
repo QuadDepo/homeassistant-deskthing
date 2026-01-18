eedf6c80ed8a3c829f0fa5895d642decdb14da54
Ryan George <ryan@qdds.nl>
2026-01-18 15:43:48 +0100

    chore: update server files to use new deskthing SDK

--- server/utils/createSettings.ts
+++ server/utils/createSettings.ts
@@ -1,46 +1,58 @@
-import { SettingsMultiSelect, SettingsString } from "deskthing-server";
+import { SETTING_TYPES, SettingOption } from "@deskthing/types";
 import { DeskThing } from "..";
 import { HassEntity } from "home-assistant-js-websocket";
 
 export const createEntitySetting = (
-	entities: HassEntity[],
-	currentEntities: string[]
+  entities: HassEntity[],
+  currentEntities: string[],
 ) => {
-	const setting: SettingsMultiSelect = {
-		label: "Select your entities",
-		description: "NOTE: some might not be supported YET",
-		type: "multiselect",
-		value: currentEntities,
-		options: entities
-			.map((entity) => entity.entity_id)
-			// NOTE: Temporary only return light entities
-			.filter((name) => name.startsWith("light."))
-			.map((name) => ({
-				value: name,
-				label: name,
-			})),
-	};
+  const options: SettingOption[] = entities
+    .map((entity) => entity.entity_id)
+    // NOTE: Temporary only return light entities
+    .filter((name) => name.startsWith("light."))
+    .map((name) => ({
+      value: name,
+      label: name,
+    }));
 
-	DeskThing.addSettings({
-		entities: setting,
-	});
+  DeskThing.setSettings({
+    entities: {
+      id: "entities",
+      label: "Select your entities",
+      description: "NOTE: some might not be supported YET",
+      type: SETTING_TYPES.MULTISELECT,
+      value: currentEntities,
+      options,
+    },
+  });
+};
+
+export const updateEntityOptions = (entities: HassEntity[]) => {
+  const options: SettingOption[] = entities
+    .map((entity) => entity.entity_id)
+    // NOTE: Temporary only return light entities
+    .filter((name) => name.startsWith("light."))
+    .map((name) => ({
+      value: name,
+      label: name,
+    }));
+
+  DeskThing.setSettingOptions("entities", options);
 };
 
 export const createBasicSettings = () => {
-	const url: SettingsString = {
-		label: "Your HomeAssistant URL",
-		type: "string",
-		value: "http://homeassistant.local:8123",
-	};
-
-	const token: SettingsString = {
-		label: "HomeAssistant Long Lived Access Token",
-		type: "string",
-		value: "",
-	};
-
-	DeskThing.addSettings({
-		url,
-		token,
-	});
+  DeskThing.initSettings({
+    url: {
+      id: "url",
+      label: "Your HomeAssistant URL",
+      type: SETTING_TYPES.STRING,
+      value: "http://homeassistant.local:8123",
+    },
+    token: {
+      id: "token",
+      label: "HomeAssistant Long Lived Access Token",
+      type: SETTING_TYPES.STRING,
+      value: "",
+    },
+  });
 };
--- server/websocketMachine.ts
+++ server/websocketMachine.ts
@@ -1,167 +1,167 @@
 import {
-	callService,
-	Connection,
-	createConnection,
-	createLongLivedTokenAuth,
-	HassEntities,
+  callService,
+  Connection,
+  createConnection,
+  createLongLivedTokenAuth,
+  HassEntities,
 } from "home-assistant-js-websocket";
 import { assign, EventObject, fromCallback, fromPromise, setup } from "xstate";
-import { subscribeEntities } from "./utils/subscribeEntities";
-import { DeskThing } from ".";
+import { subscribeEntities } from "./utils/subscribeEntities.js";
+import { DeskThing } from "./index.js";
+import WebSocket from "ws";
 
 //  NodeJS doesn't include a WebSocket client by default. We use the 'ws' package
 //  from NPM and polyfill it into the global namespace for compatibility.
-const wnd = globalThis;
-wnd.WebSocket = require("ws");
+(globalThis as any).WebSocket = WebSocket;
 
 const getAuth = fromPromise<
-	Connection,
-	{
-		url: string;
-		token: string;
-	}
+  Connection,
+  {
+    url: string;
+    token: string;
+  }
 >(async ({ input: { url, token } }) => {
-	const auth = createLongLivedTokenAuth(url, token);
-	const connection = await createConnection({ auth });
+  const auth = createLongLivedTokenAuth(url, token);
+  const connection = await createConnection({ auth });
 
-	return connection;
+  return connection;
 });
 
 const websocket = fromCallback<
-	EventObject,
-	{
-		connection: Connection;
-		entities: string[];
-	}
+  EventObject,
+  {
+    connection: Connection;
+    entities: string[];
+  }
 >(({ input: { connection, entities }, sendBack }) => {
-	const unsubscribeEntities = subscribeEntities(
-		connection,
-		(ent) => {
-			sendBack({
-				type: "ENTITIES_UPDATED",
-				entities: ent,
-			});
-		},
-		entities
-	);
+  const unsubscribeEntities = subscribeEntities(
+    connection,
+    (ent) => {
+      sendBack({
+        type: "ENTITIES_UPDATED",
+        entities: ent,
+      });
+    },
+    entities,
+  );
 
-	return () => {
-		unsubscribeEntities();
-	};
+  return () => {
+    unsubscribeEntities();
+  };
 });
 
 const websocketMachine = setup({
-	types: {
-		context: {} as {
-			url: string;
-			token: string;
-			entities: string[];
-			connection: Connection | null;
-		},
-		input: {} as {
-			url: string;
-			token: string;
-			entities: string[];
-		},
-		events: {} as
-			| {
-					type: "UPDATE_SETTINGS";
-					entities: string[];
-			  }
-			| {
-					type: "CLIENT_CONNECTED";
-			  }
-			| {
-					type: "ENTITY_ACTION";
-					action: string;
-					entity_id: string;
-			  }
-			| {
-					type: "ENTITIES_UPDATED";
-					entities: HassEntities[];
-			  },
-	},
-	actors: {
-		getAuth,
-		websocket,
-	},
+  types: {
+    context: {} as {
+      url: string;
+      token: string;
+      entities: string[];
+      connection: Connection | null;
+    },
+    input: {} as {
+      url: string;
+      token: string;
+      entities: string[];
+    },
+    events: {} as
+      | {
+          type: "UPDATE_SETTINGS";
+          entities: string[];
+        }
+      | {
+          type: "CLIENT_CONNECTED";
+        }
+      | {
+          type: "ENTITY_ACTION";
+          action: string;
+          entity_id: string;
+        }
+      | {
+          type: "ENTITIES_UPDATED";
+          entities: HassEntities[];
+        },
+  },
+  actors: {
+    getAuth,
+    websocket,
+  },
 }).createMachine({
-	id: "websocket",
-	context: ({ input: { url, token, entities } }) => ({
-		url: url,
-		token: token,
-		entities: entities,
-		connection: null,
-	}),
-	initial: "connecting",
-	states: {
-		connecting: {
-			invoke: {
-				input: ({ context }) => ({
-					...context,
-				}),
-				src: "getAuth",
-				onDone: {
-					actions: [
-						assign(({ context, event: { output } }) => ({
-							...context,
-							connection: output,
-						})),
-						() => {
-							DeskThing.sendLog("[HA] Connected");
-						},
-					],
-					target: "connected",
-				},
-				onError: {
-					actions: [
-						({ event }) => {
-							DeskThing.sendLog("[HA] Error connecting to WebSocket");
-							DeskThing.sendLog(`${event.error}`);
-						},
-					],
-				},
-			},
-		},
-		connected: {
-			entry: () => DeskThing.sendLog("[HA] Subscribing to entities"),
-			invoke: {
-				input: ({ context: { connection, entities } }) => ({
-					// Why??
-					connection: connection as Connection,
-					entities,
-				}),
-				src: "websocket",
-			},
-			on: {
-				ENTITIES_UPDATED: {
-					actions: ({ event }) => {
-						DeskThing.sendDataToClient({
-							type: "homeassistant_data",
-							payload: event.entities,
-						});
-					},
-				},
-				ENTITY_ACTION: {
-					actions: ({ context, event }) => {
-						if (!context.connection) {
-							return;
-						}
+  id: "websocket",
+  context: ({ input: { url, token, entities } }) => ({
+    url: url,
+    token: token,
+    entities: entities,
+    connection: null,
+  }),
+  initial: "connecting",
+  states: {
+    connecting: {
+      invoke: {
+        input: ({ context }) => ({
+          ...context,
+        }),
+        src: "getAuth",
+        onDone: {
+          actions: [
+            assign(({ context, event: { output } }) => ({
+              ...context,
+              connection: output,
+            })),
+            () => {
+              // console.log("[HA] Connected");
+            },
+          ],
+          target: "connected",
+        },
+        onError: {
+          actions: [
+            ({ event }) => {
+              // console.log("[HA] Error connecting to WebSocket");
+              // console.log(`${event.error}`);
+            },
+          ],
+        },
+      },
+    },
+    connected: {
+      //   entry: () => console.log("[HA] Subscribing to entities"),
+      invoke: {
+        input: ({ context: { connection, entities } }) => ({
+          // Why??
+          connection: connection as Connection,
+          entities,
+        }),
+        src: "websocket",
+      },
+      on: {
+        ENTITIES_UPDATED: {
+          actions: ({ event }) => {
+            DeskThing.send({
+              type: "homeassistant_data",
+              payload: event.entities,
+            });
+          },
+        },
+        ENTITY_ACTION: {
+          actions: ({ context, event }) => {
+            if (!context.connection) {
+              return;
+            }
 
-						const [domain, service] = event.action.split("/");
+            const [domain, service] = event.action.split("/");
 
-						callService(context.connection, domain, service, {
-							entity_id: event.entity_id,
-						});
-					},
-				},
-				CLIENT_CONNECTED: {
-					target: "connected",
-					reenter: true,
-				},
-			},
-		},
-	},
+            callService(context.connection, domain, service, {
+              entity_id: event.entity_id,
+            });
+          },
+        },
+        CLIENT_CONNECTED: {
+          target: "connected",
+          reenter: true,
+        },
+      },
+    },
+  },
 });
 
 export default websocketMachine;
