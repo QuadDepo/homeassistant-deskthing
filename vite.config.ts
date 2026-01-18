248acaf9883a03ef1d981a37be60272cc58be3d3
Ryan George <ryan@qdds.nl>
2026-01-18 21:04:07 +0100

    chore: remove legacy plugin and clean up vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import legacy from '@vitejs/plugin-legacy';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
-  plugins: [react(),
-            legacy({
-            targets: ['Chrome 69'], // Specify the browsers you want to support
-        }),],
+  base: "./",
+  plugins: [react()],
   build: {
-    outDir: 'dist',
+    // Target older Chrome for DeskThing device compatibility
+    target: "es2017",
+    outDir: "dist",
     rollupOptions: {
       output: {
-        assetFileNames: '[name]-[hash][extname]',
-        chunkFileNames: '[name]-[hash].js',
-        entryFileNames: '[name]-[hash].js',
+        assetFileNames: "[name]-[hash][extname]",
+        chunkFileNames: "[name]-[hash].js",
+        entryFileNames: "[name]-[hash].js",
       },
-    }
-  }
-})
+    },
+  },
+});
