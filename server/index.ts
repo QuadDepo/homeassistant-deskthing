a696be81572524e23dad15a31a0878f88db5680e
Ryan George <ryan@qdds.nl>
2026-01-18 21:06:16 +0100

    chore: refactor DeskThing initialization and improve status handling in start function

--- server/index.ts
+++ server/index.ts
@@ -1,15 +1,25 @@
 import { normalizeSystemStateValue } from "./utils/normalizeSystemStateValue.js";
-import { DeskThing } from "@deskthing/server";
+import { createDeskThing } from "@deskthing/server";
 import { createActor } from "xstate";
 import { systemMachine } from "./systemMachine.js";
 import { normalizeSettings } from "./utils/normalizeSettings.js";
-import { SocketData } from "@deskthing/types";
-export { DeskThing };
+import { createBasicSettings } from "./utils/createSettings.js";
+import { DESKTHING_EVENTS, SocketData } from "@deskthing/types";
+
+const DeskThing = createDeskThing();
 
 const start = async () => {
-  let data = await DeskThing.getSettings();
+  console.log("[HA] Starting Home Assistant app...");
 
-  const { url, token, entities } = normalizeSettings(data);
+  await createBasicSettings();
+
+  console.log("[HA] Settings schema initialized");
+
+  const settings = await DeskThing.getSettings();
+
+  console.log("[HA] Settings", JSON.stringify(settings, null, 2));
+
+  const { url, token, entities } = normalizeSettings(settings);
 
   const systemActor = createActor(systemMachine, {
     input: {
@@ -19,20 +29,28 @@ const start = async () => {
     },
   }).start();
 
-  // Send status on state changes
   systemActor.subscribe((state) => {
+    const status = normalizeSystemStateValue(state);
+    console.log("[HA Server] State changed, sending SERVER_STATUS:", status);
     DeskThing.send({
       type: "SERVER_STATUS",
-      payload: normalizeSystemStateValue(state),
+      payload: status,
     });
   });
 
-  // Handle client requests for current status
+  console.log("[HA Server] Registering 'get' handler for status requests");
+
   DeskThing.on("get", (socket: SocketData) => {
+    console.log("[HA Server] Received 'get' request:", JSON.stringify(socket));
+
     if (socket.request === "status") {
+      const status = normalizeSystemStateValue(systemActor.getSnapshot());
+
+      console.log("[HA Server] Responding with SERVER_STATUS:", status);
+
       DeskThing.send({
         type: "SERVER_STATUS",
-        payload: normalizeSystemStateValue(systemActor.getSnapshot()),
+        payload: status,
       });
     }
   });
@@ -43,7 +61,7 @@ const stop = async () => {
 };
 
 // Main Entrypoint of the server
-DeskThing.on("start", start);
+DeskThing.on(DESKTHING_EVENTS.START, start);
 
 // Main exit point of the server
-DeskThing.on("stop", stop);
+DeskThing.on(DESKTHING_EVENTS.STOP, stop);
